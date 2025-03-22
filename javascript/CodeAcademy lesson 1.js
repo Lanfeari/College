71a8b666d9184346baaf80dfb250b6acea978cd5
MaywellPanda <curtis.owenswork@gmail.com>
2025-03-22 21:11:45 +0000

    Add files via upload
    This is a example of some simple code i have done for college

---
+++ javascript/CodeAcademy lesson 1.js
@@ -0,0 +1,37 @@
+console.log(7); // This is the first line
+
+/* console.log(5); this is a check to see for
+multi line comments
+ and this closes it */
+
+console.log('This is checking if my multi line comment works'); // double checking the multiline comment 
+console.log('checking autosave time');
+
+console.log('JavaScript'); // This is a string
+console.log(2011); // this is an INT
+console.log('Woohoo! I love to code! #codecademy'); // printing a more complext string
+console.log(20.49) // printing a float
+
+
+console.log(3.5  + 25); // Addition
+console.log(2025 - 1969); // Subtraction
+console.log(65 / 240); // Division
+console.log(0.2708 * 100); //Multiplication
+
+console.log('Hello' + 'World'); // conceating two strings
+console.log('Hello' + ' ' + 'World'); // conceating 3 strings using the second string as an empty space
+
+console.log('Teaching the world how to code'.length); // Using the dot operator with length to retirve and then log the properties of the given string 
+
+
+
+console.log('Codecademy'.toUpperCase()); // Uses the toUpperCase method to print string in upper case
+console.log('    Remove whitespace   '.trim()); // Uses the Trim method to remove an whitespaces from the given string
+
+console.log(Math.floor(Math.random() * 100)); // Using the built in Maths object
+console.log(Math.ceil(43.8)); // Using the build in Maths object to round to the nearest Whole number
+console.log(Number.isInteger(2017)); // Checks if input is INT and returns Boolean True if it is and false if it is not.
+
+
+
+
---
+++ javascript/psedu task 2.js
@@ -0,0 +1,5 @@
+ask user for Number
+set user input to X
+perform x^3
+set output of x^3 to c
+print c to user
